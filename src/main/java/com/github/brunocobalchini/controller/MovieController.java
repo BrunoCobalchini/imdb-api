eca0e74802a40bdf72a42e21e3a9a908379114d2
Gabriel <Raoni.Gabriel@mastercard.com>
2020-04-16 09:45:02 -0300

    first version, skeleton to be implemented

---
+++ src/main/java/com/github/brunocobalchini/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.github.brunocobalchini.controller;
+
+public class MovieController {
+	
+	// TODO: private static Map<String,Movie> MOVIES = ...
+
+	// TODO: populate() {}
+	// tt4912910, Mission: Impossible - Fallout, 27/JUL/2018, 7.7, 2h27m
+	// tt0172495, Gladiator, 05/MAY/2000, 8.5, 2h35m
+	// tt7286456, Joker, 04/OUT/2019, 8.5, 2h02m
+	// tt4154796, Avengers: Endgame, 26/APR/2019, 8.4, 3h01m
+	// tt0762107, I Now Pronounce You Chuck & Larry, 20/JUL/2007, 5.9, 1h55m
+	
+	// TODO: Implement GET /movies
+	// TODO: Implement GET /movies/{id}
+	// TODO: Implement POST /movies/{id}
+	// TODO: Implement PUT /movies/{id}
+	// TODO: Implement DELETE /movies/{id}
+	
+}
